2e61994e9a75c19bf8fc0504652aa4a9b07f6839
Piotr Komosa <komosa.piotr@gmail.com>
2019-10-10 22:12:04 +0200

    commit2 - excersise2

--- src/exversise2/Device.java
+++ src/exversise2/Device.java
@@ -1,10 +1,44 @@
 package exversise2;
 
-class Device {
+public class Device {
+
     private String code;
     private String producer;
     private String type;
-    double price;
+    private double price;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
     public Device(String code, String producer, String type) {
         this.code = code;
         this.producer = producer;
@@ -13,8 +47,6 @@ class Device {
     public Device(String code, String producer, String type, double price) {
         this(code, producer, type);
         this.price = price;
-    }
-    public static void main(String[] args) {
     }
     String getInfo() {
         return "Kod: " + code + " producent: " + producer + " typ: " + type + " cena: " + price;
--- src/exversise2/Warehouse.java
+++ src/exversise2/Warehouse.java
@@ -3,11 +3,11 @@ package exversise2;
 public class Warehouse {
     public static void main(String[] args) {
 
-        Device device1 = new Device("QE65Q7FAM", "Samsung", "TV", (int) 9499.99); //code, producer, type, price
+        Device device1 = new Device("QE65Q7FAM", "Samsung", "TV", 9499.99); //code, producer, type, price
         Device device2 = new Device("P9 Lite", "Huawei", "Smatyphone"); //code, producer, type
-        Device device3 = new Device("Playstation 4", "Sony", "Smatyphone", (int) 1899.99); //code, producer, type, price
-        System.out.println(device1.allDevices()+
-                           device2.allDevices()+
-                           device3.allDevices());
+        Device device3 = new Device("Playstation 4", "Sony", "Smatyphone", 1899.99); //code, producer, type, price
+
+        String allDevices = device1.getInfo() + "\n" + device2.getInfo() + "\n" + device3.getInfo();
+        System.out.println(allDevices);
     }
 }
